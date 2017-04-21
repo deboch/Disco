0c3f989eb31d1079b6491ac6f906006b068a62cd
Debora Chamorro <dschamorro_90@hotmail.com>
2017-04-21 17:16:00 -0300

    Primer commit

---
+++ src/ar/edu/unlam/pb2/disco/Disco.java
@@ -0,0 +1,36 @@
+package ar.edu.unlam.pb2.disco;
+
+public class Disco {
+
+	private Double radioInterior;
+	private Double radioExterior;
+	private final Double PI=3.14;
+	
+	
+	public Disco(Double radioInterior, Double radioExterior ){
+		this.radioExterior=radioExterior;
+		this.radioInterior=radioInterior;
+		
+	}
+   
+	public Double getRadioInterior(){
+		return this.radioInterior;
+		
+	}
+	
+	public Double getRadioExterior(){
+		return this.radioExterior;
+		
+	}
+	public Double getPerimetroInterior(){
+		return 2*PI*radioInterior;
+	}
+	
+    public Double getPerimetroExterior(){
+		return 2*PI*radioExterior;
+	}
+    
+    public Double getSuperficie(){
+    	return PI*(radioExterior*radioExterior); 
+    }
+}
